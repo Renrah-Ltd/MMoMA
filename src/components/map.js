1dadb983716df706903f007406bdaa4e35c00d43
Jack Harner <hello@jackharner.com>
2024-09-13 11:42:18 -0600

    Plot Markers

---
+++ src/components/map.js
@@ -0,0 +1,21 @@
+import { Link } from 'gatsby';
+import React from 'react';
+
+const Map = ({ centerX, centerZ, markers }) => {
+
+    
+    const markersMap = markers.map((marker, i) => {
+        return (<Link to={`/plot/${marker.number}`} className="marker" data-plot={marker.number} style={{ "--x": marker.relative.x + "%", "--z": marker.relative.z + "%", }}></Link>)
+    })
+
+    return (
+        <div className="map">
+            <div className="overlay">
+                {markersMap}
+            </div>
+            <iframe src={`http://homesteadcraft.mcserver.us:8123/#homesteadcraft:${centerX}:76:${centerZ}:348:0:0:0:1:flat`} />
+        </div>
+    );
+};
+
+export default Map;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,32 +3,59 @@ import { graphql, Link } from "gatsby"
 import "../styles/main.scss"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Map from "../components/map"
 
 const IndexPage = ({ data }) => {
   const plots = data.allPlotsJson.nodes
 
-  const sd_coords = [[3317, -1933], [3721, -1502]]
+  const sd_coords = [[3344, -1874], [3715, -1507]]
 
-  const centerX = (sd_coords[0][0] + sd_coords[1][0]) / 2
-  const centerZ = (sd_coords[0][1] + sd_coords[1][1]) / 2
+
+  const getRelativePosition = (x, z, box) => {
+    const percentageX = ((x - box[0][0]) / (box[1][0] - box[0][0])) * 100;
+    const percentageZ = ((z - box[0][1]) / (box[1][1] - box[0][1])) * 100;
+    console.log({ x, z, percentageX, percentageZ })
+    return { x: percentageX, z: percentageZ };
+  }
+
+  const getCenter = (coords) => {
+    return { x: (coords[0][0] + coords[1][0]) / 2, z: (coords[0][1] + coords[1][1]) / 2 }
+  }
+
+  const SDcenter = getCenter(sd_coords)
+
+  console.log({ plots })
+  const markers = plots.map((plot) => {
+    const plotCenter = getCenter([Object.values(plot.start_coords), Object.values(plot.end_coords)])
+    console.log({ plot, plotCenter })
+    return ({
+      number: plot.plotNumber,
+      point: plotCenter,
+      relative: getRelativePosition(plotCenter.x, plotCenter.z, sd_coords)
+    })
+  })
+
+  console.log({ markers })
+
+  
 
   return (
     <Layout>
-    <Seo title="Welcome" />
+      <Seo title="Welcome" />
 
-        <div class="grid grid--2 grid--mobile-1">
-          <div className="map"><iframe src={`http://homesteadcraft.mcserver.us:8123/#homesteadcraft:${centerX}:76:${centerZ}:348:0:0:0:1:flat`} /></div>
-          <div className="sd-info">
-            <h1>Shopping District</h1>
-            <ul className="plots-index">
-              {plots.map(plot => (
-                <li key={plot.plotNumber}>
-                  <Link to={`/plot/${plot.plotNumber}`}>{plot.shopName ? plot.shopName : `Plot #${plot.plotNumber}`}</Link> - {plot.owner}
-                </li>
-              ))}
-            </ul>
-          </div>
+      <div class="grid grid--2 grid--mobile-1">
+        <Map centerX={SDcenter.x} centerZ={SDcenter.z} markers={markers} />
+        <div className="sd-info">
+          <h1>Shopping District</h1>
+          <ul className="plots-index">
+            {plots.map(plot => (
+              <li className="plot-card" key={plot.plotNumber} data-plot={plot.plotNumber}>
+                <Link to={`/plot/${plot.plotNumber}`}>{plot.shopName ? plot.shopName : `Plot #${plot.plotNumber}`}</Link> - {plot.owner}
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
     </Layout>
   )
 }
@@ -40,6 +67,13 @@ export const query = graphql`
         plotNumber
         shopName
         owner
+        start_coords{
+        x
+        z
+        }
+        end_coords{
+        x
+        z}
       }
     }
   }
