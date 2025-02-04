74370e58c89da5df899cf06410130dd0d50c8bc6
Jack Harner <jack@jackharner.com>
2025-02-03 23:20:48 -0700

    Deploy

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,9 +6,4 @@
 
 // You can delete this file if you're not using it
 
-exports.onInitialClientRender = () => {
-    if (typeof window !== 'undefined' && window.location.protocol === 'https:') {
-        const newUrl = window.location.href.replace('https:', 'http:');
-        window.location.replace(newUrl);
-    }
-};
+
---
+++ public/component---src-templates-art-template-js-2351b01f504e37fe936c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[12],{6855:function(e,t,a){a.r(t);var r=a(6540),l=(a(4794),a(4333),a(6025)),n=a(7528);t.default=e=>{let{data:t,pageContext:a}=e;const{art:s}=a;return r.createElement(l.A,{className:"art-page art-single"},r.createElement(n.A,{title:s.name}),r.createElement("img",{src:s.image}),r.createElement("h1",null,s.name))}}}]);
+//# sourceMappingURL=component---src-templates-art-template-js-2351b01f504e37fe936c.js.map
